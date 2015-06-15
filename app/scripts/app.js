c5e35c6844657b32529bf7bbf6ba7449570eb53d
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-06-15 00:00:53 -0400

    basic app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pushNotificationsApp
+ * @description
+ * # pushNotificationsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pushNotificationsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pushNotificationsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pushNotificationsApp
+ */
+angular.module('pushNotificationsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
